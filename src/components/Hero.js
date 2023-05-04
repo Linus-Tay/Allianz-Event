ee2c40b479a204d108f0882d028c0e9754f21499
Linus-Tay <82197955+Linus-Tay@users.noreply.github.com>
2023-05-04 11:27:00 +0800

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,8 +8,8 @@ const Hero = () => {
         <div className='text-center content'>
         <Fade cascade triggerOnce>
         <p className='text-orange-400 special text-6xl font-bold leading-snug'>Staff Appreciation Day 2023</p>
-        <p className='text-white nicer text-xl italic pt-10'>Valuing your commitment is our pleasure.</p>
-        </Fade>
+        <p className='text-white nicer text-xl italic pt-10 font-extrabold'>Valuing your commitment is our pleasure.</p>
+        </Fade>https://github.com/Linus-Tay/STTelemedia-Event/edit/master/src/components/Hero.js
         </div>
     </div>
   )
