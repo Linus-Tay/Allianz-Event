ee76d07c3015e954a604cc22238dfce630ce9787
Linus-Tay <regimelt@hotmail.com>
2023-05-04 20:43:27 +0800

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Fade } from "react-awesome-reveal";
 import './Hero.css'
+import Sad23 from './images/SAD23.png'
 
 const Hero = () => {
   return (
     <div className='relative hero' id='hero'>
         <div className='text-center content'>
         <Fade cascade triggerOnce>
-        <p className='text-orange-400 special text-6xl font-bold leading-snug'>Staff Appreciation Day 2023</p>
+          <img className='m-auto' src={Sad23} alt=''/>
         <p className='text-black text-xl italic pt-10 font-bold'>Valuing your commitment is our pleasure.</p>
         </Fade>
         </div>
