6503ac4b1c38f58ff55b6a98b5e63093af7618cf
Linus-Tay <regimelt@hotmail.com>
2023-05-08 01:06:53 +0800

    test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Registration from './components/Registration';
+import Error from './components/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,7 @@ root.render(
     <Routes>
       <Route exact path='/' element={< App />}></Route>  
       <Route exact path='/registration' element={<Registration />}></Route>
+      <Route path='*' element={< Error />}></Route>  
     </Routes>
   </Router>
 );
