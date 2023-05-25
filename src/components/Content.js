652e6b702fc53c08a31db96d705a627d1e2901cb
Linus-Tay <82197955+Linus-Tay@users.noreply.github.com>
2023-05-25 12:23:50 +0800

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,7 +7,7 @@ const Content = () => {
             <p style={{ letterSpacing: '3px '}} className='text-red-500 text-sm font-bold sttelemedia text-center'>ABOUT THE EVENT</p>
             <p style={{ color: '#5a5a5a' }} className='pt-4 sttelemedia text-4xl md:text-5xl font-bold text-center '>[RE]Connect</p>
             <p style={{ color: '#5a5a5a' }} className='text-neutral-600 sttelemedia text-2xl pt-4 md:text-3xl font-bold text-center'>"Rekindling Connections: Staff Appreciation Day"</p>
-            <p style={{ color: '#5a5a5a' }} className='pt-8 text-xl text-neutral-600'>
+            <p style={{ color: '#5a5a5a' }} className='pt-8 text-xl text-center text-neutral-600'>
                 Colleagues will convene for a special staff retreat to reestablish the ties that have driven our success. Let's set off on a voyage of reconnection, and rediscovery of friendships in the peaceful embrace of nature, amidst soaring trees and exquisite beaches of Sentosa.
                 <br/><br/>
                 The day starts out with team-building activities that test boundaries and promote cooperation. Let's re-connect on a deeper level through engaging challenges and shared successes. Together, overcome challenges by relying on mutual trust, clear communication, and group problem-solving abilities. Laughter, inspiration, and a rekindled sense of friendship permeate the space. Realise the value of teamwork and the strength you have as a cohesive unit.
