146457acbd5405498156c7f04e4c4783c8be1f50
Linus-Tay <regimelt@hotmail.com>
2023-05-31 21:21:29 +0800

    yessir

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className='bg-gray-900' id='about'>
         <div className="text-gray-200 grid grid-cols-12 gap-4 p-10">
             <div className='p-10 col-span-12 md:col-span-6'>
-                <p className='sttelemedia text-red-600 pb-2 text-3xl font-bold '>ABOUT</p>
+                <p className='sttelemedia text-red-600 pb-2 text-xl font-bold '>ABOUT</p>
                 <p className='sttelemedia text-lg font-light'>Let's set off on a voyage of reconnection, and rediscovery of friendships in the peaceful embrace of nature, admist soaring trees and exquisite beaches of Sentosa.
                 </p>
             </div>
