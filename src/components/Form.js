78df623f4d07d9f718bb7f1f79c68ee64173fb5c
Linus-Tay <82197955+Linus-Tay@users.noreply.github.com>
2023-06-06 12:47:34 +0800

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -161,7 +161,7 @@ export const Test = () => {
         </div>
         <div className="relative z-0 w-full mb-6 group">
             <input name="email" type="email" onInput={(e) => setFormData({...formData, email: e.target.value})} value={formData.email} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none    focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-            <label for="floating_email" className="sttelemedia peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus: peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ST Telemedia Email Address</label>
+            <label for="floating_email" className="sttelemedia peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus: peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">STT GDC Email Address</label>
         </div>     
         
         <fieldset>
