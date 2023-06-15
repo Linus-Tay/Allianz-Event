b1e92defa5b630156c49d302c44ebbfea6b6de52
Linus-Tay <82197955+Linus-Tay@users.noreply.github.com>
2023-06-15 14:02:41 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,9 @@ const Footer = () => {
                 <span className='line'></span>
                 <div className='flex sttelemedia justify-center container m-auto'>
                   <p className='flex-initial'><AiFillLinkedin size={26}/></p>
-                  <p className='flex-auto'>Share your memorable stories and experiences at STT GDC on LinkedIn and tag us 
-<span><a className="underline" href="https://www.linkedin.com/company/sttgdc" target="_blank">@ST Telemedia Global Data Centre!</a></span></p>
-                
+                  <p className='flex-auto'>Share your memorable stories and experiences at STT GDC on LinkedIn and tag us <span><a className="underline" href="https://www.linkedin.com/company/sttgdc" target="_blank">@ST Telemedia Global Data Centre!</a></span></p>                
     </div>
+    <br/>
     <p className="sttelemedia">#BuiltForFun #LifeatSTTGDC #EnablingOurDigitalFuture</p>
                 <br/>
                  <p className='sttelemedia italic text-sm'>© 2023 STT GDC Pte Ltd</p>
