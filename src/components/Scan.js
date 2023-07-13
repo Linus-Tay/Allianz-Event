9242b5fd74f6c5d898d9d954c93b410b8f065c13
Linus-Tay <regimelt@hotmail.com>
2023-07-14 00:38:38 +0800

    test

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -189,7 +189,7 @@ const Scan = () => {
         .then((snapshot) => {
             let information = []
             snapshot.docs.forEach((doc) => {
-              information.push({UNIQUE_NUMBER: doc.data().uniqueNumber, FULL_NAME: doc.data().formData.name, NRIC: doc.data().formData.nric, PHONE_NUMBER: doc.data().formData.phoneNumber, EMAIL: doc.data().formData.email, ATTENDING: doc.data().formData.attending, ACCOMMODATION: doc.data().formData.accommodation,  PARKING: doc.data().formData.parking,
+              information.push({UNIQUE_NUMBER: doc.data().uniqueNumber, FULL_NAME: doc.data().formData.name, TEAM_NUMBER: doc.data().teamNumber, NRIC: doc.data().formData.nric, PHONE_NUMBER: doc.data().formData.phoneNumber, EMAIL: doc.data().formData.email, ATTENDING: doc.data().formData.attending, ACCOMMODATION: doc.data().formData.accommodation,  PARKING: doc.data().formData.parking,
                 KAYAK_SINGLE: doc.data().formData.kayakSingle, KAYAK_DOUBLE: doc.data().formData.kayakDouble, DONUT_RIDE: doc.data().formData.donutRide, STAND_UP_PADDING_BOARDING: doc.data().formData.standUpPaddleBoarding, BANANA_BOAT_RIDE: doc.data().formData.bananaBoatRide,
                 T_SHIRT: doc.data().formData.tShirt, TANK_TOP: doc.data().formData.tankTop,
                 DIETARY_RESTRICTIONS: doc.data().formData.dietaryRestriction, REGISTERED: doc.data().registered})
