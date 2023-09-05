bce626365de2d42b736c447a5846ead17c143b8e
Linus Tay <s10195119@connect.np.edu.sg>
2023-09-05 08:04:42 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Registration from './components/Registration';
+
 import Error from './components/Error';
 import Scan from './components/Scan';
 import Screen from './components/Screen';
@@ -13,9 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-      <Route exact path='/' element={< App />}></Route>  
-      <Route exact path='/registration' element={<Registration />}></Route>
-      <Route exact path='/scan' element={<Scan />}></Route>
+      <Route exact path='/' element={< Scan />}></Route>  
       <Route exact path='/screen' element={<Screen />}></Route>
       <Route exact path='/edit' element={<Edit />}></Route>
       <Route path='*' element={< Error />}></Route>  
